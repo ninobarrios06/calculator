9fe7f3a1382643205b1365b61434f78bc7f01f9b
ninobarrios <nicolas6x@hotmail.com>
2025-10-27 11:02:10 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,209 @@
+function calculateSimple() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+
+    if (isNaN(num1) || isNaN(num2) || num2 === 0) {
+        document.getElementById("resultado").value = "Error";
+        document.getElementById("tablaNum1").textContent = "-";
+        document.getElementById("tablaNum2").textContent = "-";
+        document.getElementById("tablaResultado").innerHTML = "<math><mi>x</mi></math>";
+        document.getElementById("formula").innerHTML = "";
+        return;
+    }
+
+    const resultado = (num1 * num2) / 100;
+
+    document.getElementById("resultado").value = resultado.toFixed(2);
+
+    document.getElementById("tablaNum1").textContent = num1;
+    document.getElementById("tablaNum2").textContent = num2;
+    document.getElementById("tablaResultado").innerHTML = `<math><mi>x</mi></math>`;
+
+    document.getElementById("formula").innerHTML = `
+        <math display="block">
+          <mi>x</mi>
+          <mo>=</mo>
+          <mfrac>
+            <mrow>
+              <mn>${num1}</mn>
+              <mo>&#x22C5;</mo>
+              <mn>${num2}</mn>
+            </mrow>
+            <mn>100</mn>
+          </mfrac>
+        </math>
+        <math display="block" class="mt-3">
+          <mi>x</mi>
+          <mo>=</mo>
+          <mn>${resultado.toFixed(2)}</mn>
+        </math>
+    `;
+    const graphicSimplePercentage = document.querySelector(".graphicSimplePercentage");
+    const graphicSimplePercentageTotal = document.querySelector(".graphicSimplePercentageTotal");
+    const graphicSimplePercentage2 = document.querySelector(".graphicSimplePercentage2");
+    const graphicSimplePercentageTotal2 = document.querySelector(".graphicSimplePercentageTotal2");
+    const totalGraphic = document.querySelector(".totalGraphic");
+
+    graphicSimplePercentage.style.width = "0%";
+    graphicSimplePercentageTotal.style.width = "0%";
+    setTimeout(() => {
+        graphicSimplePercentage.style.width = `${num1}%`;
+        graphicSimplePercentage2.style.width = `${num1}%`;
+        graphicSimplePercentageTotal.style.width = `${100 - num1}%`;
+        graphicSimplePercentageTotal2.style.width = `${100 - num1}%`;
+
+        graphicSimplePercentage.textContent = `${num1}%`;
+        graphicSimplePercentageTotal.textContent = `${100 - num1}%`;
+        graphicSimplePercentage2.textContent = `${resultado}`;
+        graphicSimplePercentageTotal2.textContent = `${(num2 - resultado).toFixed(2)}`;
+        totalGraphic.textContent = `Total : ${num2}`;
+
+    }, 100);
+}
+
+
+
+
+function calculateTotal() {
+    const num1Total = parseFloat(document.getElementById("num1Total").value);
+    const num2Total = parseFloat(document.getElementById("num2Total").value);
+    const resulTotal = (num2Total * 100) / num1Total;
+
+    const graphicSimplePercentageTotalTotal = document.querySelector('.graphicSimplePercentageTotalTotal');
+    const graphicSimplePercentagetotal = document.querySelector('.graphicSimplePercentagetotal');
+    const graphicSimplePercentageTotal2total = document.querySelector('.graphicSimplePercentageTotal2total');
+    const totalGraphicTotal = document.querySelector('.totalGraphicTotal');
+    const graphicSimplePercentageTotal2total1 = document.querySelector('.graphicSimplePercentageTotal2total1');
+
+    document.getElementById("resulTotal").value = resulTotal.toFixed(2);
+    document.getElementById("tablaNum1Total").textContent = num1Total;
+    document.getElementById("tablaNum2Total").textContent = num2Total;
+    document.getElementById("formulaTotal").innerHTML = `
+      <math display="block">
+        <mi>x</mi>
+        <mo>=</mo>
+        <mfrac>
+          <mrow>
+            <mn>${num2Total}</mn>
+            <mo>&#x22C5;</mo>
+            <mn>100</mn>
+          </mrow>
+          <mn>${num1Total}</mn>
+        </mfrac>
+      </math>
+      <math display="block" class="mt-3">
+        <mi>x</mi>
+        <mo>=</mo>
+        <mn>${resulTotal.toFixed(2)}</mn>
+      </math>
+  `;
+    console.log(num1Total, "++++", num2Total, "++++", resulTotal);
+    setTimeout(() => {
+        graphicSimplePercentageTotal2total.style.width = `${100 - num1Total}%`;
+        graphicSimplePercentageTotalTotal.style.width = `${100 - num1Total}%`;
+        graphicSimplePercentagetotal.style.width = `${num1Total}%`;
+        graphicSimplePercentageTotal2total1.style.width = `${num1Total}%`;
+
+        graphicSimplePercentageTotal2total.textContent = `${(resulTotal - (((num1Total * resulTotal) / 100))).toFixed(2)}`;
+        graphicSimplePercentageTotalTotal.textContent = `${100 - num1Total}%`;
+        graphicSimplePercentagetotal.textContent = `${((num1Total * resulTotal) / 100).toFixed(2)}`;//--------
+        graphicSimplePercentageTotal2total1.textContent = `${(num1Total).toFixed(1)}%`;
+        totalGraphicTotal.textContent = `Total : ${resulTotal.toFixed(2)}`;
+    }, 100);
+}
+
+function calculateAmount() {
+    const amount1 = parseFloat(document.getElementById("amount1").value);
+    const amount2 = parseFloat(document.getElementById("amount2").value);
+    const amountResult = (amount2 * 100) / amount1;
+    document.getElementById("formulaAmount1").textContent = amount2;
+    document.getElementById("formulaAmount2").textContent = amount1;
+    document.getElementById("amountResult").value = amountResult.toFixed(2);
+    document.getElementById("formulaAmount").innerHTML = `
+      <math display="block">
+        <mi>x</mi>
+        <mo>=</mo>
+        <mfrac>
+          <mrow>
+            <mn>${amount2}</mn>
+            <mo>&#x22C5;</mo>
+            <mn>100</mn>
+          </mrow>
+          <mn>${amount1}</mn>
+        </mfrac>
+      </math>
+      <math display="block" class="mt-3">
+        <mi>x</mi>
+        <mo>=</mo>
+        <mn>${amountResult.toFixed(2)}%</mn>
+      </math>
+  `;
+    const graphichAmount1 = document.querySelector('.graphichAmount1');
+    const graphichAmount2 = document.querySelector('.graphichAmount2');
+    const graphichAmount3 = document.querySelector('.graphichAmount3');
+    const graphichAmount4 = document.querySelector('.graphichAmount4');
+    const graphichAmount5 = document.querySelector('.graphichAmount5');
+
+    graphichAmount1.textContent = `Total: ${amount1}`;
+    graphichAmount2.textContent = `${amountResult.toFixed(2)}%`;
+    graphichAmount3.textContent = `${(100 - amountResult).toFixed(2)}%`;
+    graphichAmount4.textContent = `${((amountResult * amount1) / 100).toFixed(2)}`;
+    graphichAmount5.textContent = `${(amount1 - amount2).toFixed(2)}`;
+
+    graphichAmount2.style.width = `${amountResult}%`;
+    graphichAmount3.style.width = `${100 - amountResult}%`;
+    graphichAmount4.style.width = `${amountResult}%`;
+    graphichAmount5.style.width = `${100 - amountResult}%`;
+
+
+}
+
+function calculateChange() {
+    const change1 = parseFloat(document.getElementById("change1").value);
+    const change2 = parseFloat(document.getElementById("change2").value);
+    const change3 = parseFloat(document.getElementById("change3").value);
+    const changeResult = (change2 * change3) / change1;
+    console.log(change1, change2, change3, changeResult);
+    document.getElementById("formulaChange1").textContent = change2;
+    document.getElementById("formulaChange2").textContent = change1;
+    document.getElementById("formulaChange3").textContent = change3;
+
+    document.getElementById("resultChange").value = changeResult.toFixed(2);
+    document.getElementById("formulaChange").innerHTML = `
+      <math display="block">
+        <mi>x</mi>
+        <mo>=</mo>
+        <mfrac>
+          <mrow>
+            <mn>${change2}</mn>
+            <mo>&#x22C5;</mo>
+            <mn>${change3}</mn>
+          </mrow>
+          <mn>${change1}</mn>
+        </mfrac>
+      </math>
+      <math display="block" class="mt-3">
+        <mi>x</mi>
+        <mo>=</mo>
+        <mn>${changeResult.toFixed(2)}%</mn>
+      </math>
+  `;
+    const graphichChangue1 = document.querySelector('.graphichChangue1');
+    const graphichChangue2 = document.querySelector('.graphichChangue2');
+    const graphichChangue3 = document.querySelector('.graphichChangue3');
+    const graphichChangue4 = document.querySelector('.graphichChangue4');
+    const graphichChangue5 = document.querySelector('.graphichChangue5');
+    graphichChangue1.textContent = `Total: ${((change2 * 100) / change1).toFixed(1)}`;
+    graphichChangue2.textContent = `${change1.toFixed(1)}%`;
+    graphichChangue3.textContent = `${change3.toFixed(1)}%`;
+    graphichChangue4.textContent = `${change2.toFixed(1)}`;
+    graphichChangue5.textContent = `${changeResult.toFixed(1)}`;
+
+
+    graphichChangue2.style.width = `${change1}%`;
+    graphichChangue3.style.width = `${change3}%`;
+    graphichChangue4.style.width = `${change1}%`;
+    graphichChangue5.style.width = `${change3}%`;
+
+
+}
